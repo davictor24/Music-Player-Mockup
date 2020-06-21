386b39d64c5cfdc11d6de2278269df5679d78644
Victor Durojaiye <durojaiyevic@gmail.com>
2020-06-21 23:41:54 +0100

    Add song POJO

--- app/src/main/java/com/electroninc/musicplayermockup/models/Song.java
+++ app/src/main/java/com/electroninc/musicplayermockup/models/Song.java
@@ -1,4 +1,25 @@
 package com.electroninc.musicplayermockup.models;
 
 public class Song {
+    private String name;
+    private String artist;
+    private int length;
+
+    public Song(String name, String artist, int length) {
+        this.name = name;
+        this.artist = artist;
+        this.length = length;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int getLength() {
+        return length;
+    }
 }
