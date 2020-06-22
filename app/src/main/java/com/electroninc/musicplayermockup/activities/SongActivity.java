726a6aaa44e8aa0140838314e13e7b69f0011584
Victor Durojaiye <durojaiyevic@gmail.com>
2020-06-22 04:38:16 +0100

    Populate song activity view

--- app/src/main/java/com/electroninc/musicplayermockup/activities/SongActivity.java
+++ app/src/main/java/com/electroninc/musicplayermockup/activities/SongActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.electroninc.musicplayermockup.R;
 import com.electroninc.musicplayermockup.models.Song;
+import com.electroninc.musicplayermockup.utils.Utils;
 
 public class SongActivity extends AppCompatActivity {
 
@@ -20,6 +22,24 @@ public class SongActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         Song song = (Song) intent.getSerializableExtra(INTENT_SONG);
-        Toast.makeText(this, song.getName(), Toast.LENGTH_SHORT).show();
+        if (song == null) return;
+
+        // song duration, rewind, play_pause, forward
+        ImageView albumArtBackground = findViewById(R.id.album_art_background);
+        ImageView albumArt = findViewById(R.id.album_art);
+        TextView albumName = findViewById(R.id.album_name);
+        TextView nowPlaying = findViewById(R.id.now_playing);
+        TextView artistName = findViewById(R.id.artist_name);
+        TextView songDuration = findViewById(R.id.song_duration);
+        ImageView rewind = findViewById(R.id.rewind);
+        ImageView playPause = findViewById(R.id.play_pause);
+        ImageView forward = findViewById(R.id.forward);
+
+        albumArtBackground.setImageResource(song.getAlbumArt());
+        albumArt.setImageResource(song.getAlbumArt());
+        albumName.setText(song.getAlbum());
+        nowPlaying.setText(song.getName());
+        artistName.setText(song.getArtist());
+        songDuration.setText(Utils.formatDuration(song.getLength()));
     }
 }
